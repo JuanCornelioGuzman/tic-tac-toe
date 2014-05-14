b35f64bff29a32cfb7ba0337e26e02122c926f8f
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2014-05-14 21:09:31 +0800

    Renamed from TicTacToeAI to TicTacToe

---
+++ TicTacToe/src/net/jcornelio/projects/tictactoe/gameplay/ScoreManager.java
@@ -0,0 +1,13 @@
+package net.jcornelio.projects.tictactoe.gameplay;
+
+/***************************************************
+ * @author: Juan Cornelio S. Guzman
+ * @since: Oct 11, 2013
+ * @version: 1.0
+ **************************************************/
+public class ScoreManager {
+
+    
+    
+}
+
