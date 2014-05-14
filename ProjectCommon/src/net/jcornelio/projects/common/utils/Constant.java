5978d0fbd04188ce1ffac80553826f4df3494ed2
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2014-05-14 21:06:30 +0800

    Added Common Java Swing Project

---
+++ ProjectCommon/src/net/jcornelio/projects/common/utils/Constant.java
@@ -0,0 +1,13 @@
+package net.jcornelio.projects.common.utils;
+
+/***************************************************
+ * @author: Juan Cornelio S. Guzman
+ * @since: Oct 24, 2013
+ * @version: 1.0
+ **************************************************/
+public class Constant {
+    
+    public static final String BLANK = "";
+
+}
+
