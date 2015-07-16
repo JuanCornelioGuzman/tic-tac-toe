3e49b486bd24b3cbafc59765626e1018781b2763
jcornelio.guzman@gmail.com <jcornelio.guzman@gmail.com>
2015-07-16 21:03:26 +0800

    NumberConverter

---
+++ SimpleProjects/src/net/jcornelioguzman/projects/converter/util/NumberConstants.java
@@ -0,0 +1,61 @@
+package net.jcornelioguzman.projects.converter.util;
+
+/**
+ * 
+ * @author JC GUZMAN
+ */
+public final class NumberConstants {
+
+	private NumberConstants() {
+	}
+
+	public static final String ONE = " ONE";
+
+	public static final String TWO = " TWO";
+
+	public static final String THREE = " THREE";
+
+	public static final String FOUR = " FOUR";
+
+	public static final String FIVE = " FIVE";
+
+	public static final String SIX = " SIX";
+
+	public static final String SEVEN = " SEVEN";
+
+	public static final String EIGHT = " EIGHT";
+
+	public static final String NINE = " NINE";
+
+	public static final String TEN = " TEN";
+
+	public static final String ELEVEN = " ELEVEN";
+
+	public static final String TWELVE = " TWELVE";
+
+	public static final String THIRTEEN = " THIRTEEN";
+
+	public static final String FOURTEEN = " FOURTEEN";
+
+	public static final String FIFTEEN = " FIFTEEN";
+
+	public static final String SIXTEEN = " SIXTEEN";
+
+	public static final String SEVENTEEN = " SEVENTEEN";
+
+	public static final String EIGHTEEN = " EIGHTEEN";
+
+	public static final String NINETEEN = " NINETEEN";
+
+	public static final String FIF = " FIF";
+
+	public static final String TEEN = "TEEN";
+
+	public static final String TWENT = "TWEN";
+
+	public static final String TY = "TY";
+
+	public static final String HUNDRED = " HUNDRED";
+
+	public static final String THOUSAND = " THOUSAND";
+}
