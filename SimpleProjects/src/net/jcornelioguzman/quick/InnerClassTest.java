877a89c53820132a9130781d1f69af3271138e82
Juan Cornelio Guzman <jcornelio.guzman@gmail.com>
2017-02-15 20:46:14 +0800

    Removed unrelated projects

--- SimpleProjects/src/net/jcornelioguzman/quick/InnerClassTest.java
+++
@@ -1,9 +0,0 @@
-package net.jcornelioguzman.quick;
-
-public class InnerClassTest {
-	public static void main(String[] args) {
-		InnerClass b = new InnerClass();
-		System.out.println(b.b);
-
-	}
-}
